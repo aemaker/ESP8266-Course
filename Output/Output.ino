6db2c50c7ceb9ea9d9bdfa59259ab2920b2322d8
aemaker <57056040+aemaker@users.noreply.github.com>
2020-01-18 14:34:34 +0700

    Create Output.ino
    First commit

---
+++ Output/Output.ino
@@ -0,0 +1,18 @@
+// Khai báo thư viện và khai báo biến
+
+// Thiết lập
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D4, OUTPUT);
+
+}
+
+// Vòng lặp
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(D4, HIGH);
+  delay(1000);
+  digitalWrite(D4, LOW);
+  delay(1000);
+
+}
